1b82061fa86bb2b6ce3ba9d0f847a3ddfe999558
unknown <admin@pnvd244.pn.com.vn>
2023-02-07 15:29:38 +0700

    first commit

---
+++ Product/Newproduct.js
@@ -0,0 +1,21 @@
+var id=0;
+        function sell()
+        {
+            id++;
+            var pname = frm.pname.value;
+            var qty = frm.qty.value;
+            var Price = frm.up.value;
+            var dc = frm.dc.value;
+            var sub = qty*Price*(1-dc/100);  //
+            var row = "<tr>";
+                row += "<td>" + id + "</td>";
+                row += "<td>" + pname + "</td>";
+                row += "<td>" + qty + "</td>";
+                row += "<td>$" + Price + "</td>";
+                row += "<td>" + dc + "</td>";
+                row += "<td>$" + sub + "</td>";
+                row +="</tr>";
+                document.getElementById("tbl").innerHTML += row;
+        }
+
+        
